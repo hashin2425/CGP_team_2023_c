8173494d229db519b7c854c181fe2a2ea8e0017a
Haya <hayachannel5@gmail.com>
2023-09-23 09:32:22 +0900

    [Fix] ビルド時に不具合が生じた箇所を削除

--- Assets/Hayashi/Scripts/FishCatch/FishInstance.cs
+++ Assets/Hayashi/Scripts/FishCatch/FishInstance.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using static UnityEngine.GraphicsBuffer;
-using UnityEditorInternal;
 using TMPro;
 
 public class FishInstance : MonoBehaviour
